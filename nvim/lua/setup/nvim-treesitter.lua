e236af973df04567517f828b6a379b451ada6c66
NickGalindo <nickdavga@gmail.com>
2024-08-30 15:28:11 -0500

    adding java to treesitter

--- nvim/lua/setup/nvim-treesitter.lua
+++ nvim/lua/setup/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 local treesitter = require("nvim-treesitter.configs")
 treesitter.setup({
-  ensure_installed = {"c", "cpp", "python", "lua", "rust", "yaml", "json", "go", "gomod", "gosum", "gowork", "typescript", "javascript", "tsx"},
+  ensure_installed = {"c", "cpp", "python", "lua", "rust", "yaml", "json", "go", "gomod", "gosum", "gowork", "typescript", "javascript", "tsx", "java"},
   sync_install = true,
   highlight = {
     enable = true,
